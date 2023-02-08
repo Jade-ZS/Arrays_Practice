753e90fbd32974d4607f335977228ed5a087c4fd
Jade Shi <ziyushi1997@gmail.com>
2023-02-07 16:39:50 -0800

    Initial commit

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var name = ['Micky', 'Minnie', 'Bruto', 'Donald'];
+var number = [1, 2, 3, 4];
+var isFemale = true;
+var isSingle = false;
+var isRich = false;
+var isHungry = true;
+var Boolean = [isFemale, isSingle, isRich, isHungry]
